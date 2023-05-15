3dc41e8c95ed2a4e9b27b43da681c710998af535
n1colashka <deckards2505@gmail.com>
2023-05-15 13:03:09 +0600

    add home page media css

--- src/js/components/search-filter.js
+++ src/js/components/search-filter.js
@@ -4,6 +4,7 @@ const filterBody = document.querySelector('.search-filter__body');
 const filterBtn = document.querySelector('.search-filter__el');
 
 filterBtn?.addEventListener('click', () => {
+  filterBtn.classList.toggle('is-active');
   filterBody.classList.toggle('is-active');
   // AnimateElement.toggle(
   //   filterBody,
