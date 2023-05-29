e3f197268e24a52f9bf73fd179fa9cf6f119dae5
n1colashka <deckards2505@gmail.com>
2023-05-29 18:23:16 +0600

    fixes

--- src/js/components/accordions.js
+++ src/js/components/accordions.js
@@ -1,3 +1,4 @@
 import { initAccordion } from '../functions/accordion';
 
 initAccordion('.product-info__head', '.product-info__item');
+initAccordion('.accordion__head', '.accordion__item');
--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,3 +1,4 @@
 import { initTabs } from "../functions/tabs";
 
 initTabs('.catalog__tabs li', '.catalog__list');
+initTabs('.terms-tabs li', '.terms-content__item');
